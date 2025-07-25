20b77be6615d0b16f85b39f6e6c5e727b4f715b2
Aditya Kumar <aditya202666@gmail.com>
2025-07-25 10:19:29 +0530

    feat: Update axios configuration to use production URL

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-
+const productionUrl = "https://mindmesh-backend-i3pz.onrender.com/api/v1";
+const developmentUrl = "http://localhost:3030/api/v1";
 
 const axiosInstance = axios.create({
-    baseURL : " http://localhost:3030/api/v1",
+    baseURL : productionUrl,
     withCredentials: true,
 })
 
